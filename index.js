69d901798eacbeef6331df93b87db057d5cc9359
Maxime P <mpperusse@gmail.com>
2016-01-24 01:31:36 -0500

    token hardcoded

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-$.ajax({
-  url: 'https://api.twitter.com/oauth/request_token',
-  type: 'POST',
-  beforeSend: function (xhr) {
-      xhr.setRequestHeader('oauth_consumer_key', "qco0kTVO5GHh37T7FKeI4ACSa");
-      xhr.setRequestHeader('oauth_token', "2582787825-eMclz0Yc2bcPWqoHUMKk4NpyQrUmVkkg2qREgh0");
-  },
-  dataType: 'jsonp',
-  success: function (data) {
-      alert(data);
-  }
-});
 
-$(function() {
-  var x = 0;
-  $.getJSON("https://api.twitter.com/1.1/trends/place.json?id=1", function(result) {
-      console.log(++x);
-      $.each(result, function(i, field) {
 
-            console.log(field);
-      });
-  });
+// var R = require("request");
+
+// var key = 'qco0kTVO5GHh37T7FKeI4ACSa';
+// var secret = 'foqwPKYifY0TP2ljQDtDLVc6mQ6tj0dZyXFr18FioLHcLFNqnj';
+// var cat = key +":"+secret;
+// var credentials = new Buffer(cat).toString('base64');
+
+// var url = 'https://api.twitter.com/oauth2/token';
+
+// R({ url: url,
+//     method:'POST',
+//     headers: {
+//         "Authorization": "Basic " + credentials,
+//         "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
+//     },
+//     body: "grant_type=client_credentials"
+
+// }, function(err, resp, body) {
+
+//     console.dir(body); //the bearer token...
+
+// });
+
+// $(function() {
+//   var x = 0;
+//   $.getJSON("https://api.twitter.com/1.1/trends/place.json?id=1", function(result) {
+//       console.log(++x);
+//       $.each(result, function(i, field) {
+
+//             console.log(field);
+//       });
+//   });
 
   // var socket = new WebSocket('');
 
@@ -37,5 +49,5 @@ $(function() {
   // }
 
 
-});
+// });
 
